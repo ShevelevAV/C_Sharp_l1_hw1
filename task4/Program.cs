fa15bc921b1f7d76d39eb441a8f089a62d706f46
Aleksandr Shevelev <leksevi@icloud.com>
2022-12-02 03:27:23 +0300

    Задача №8

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+Console.WriteLine("Введите число а: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число c: ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+Console.WriteLine("max = " + max);
--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+double i = N % 2;
+if(i == 0){
+    Console.WriteLine("Четное");
+}else{
+    Console.WriteLine("Нечетное");
+}
